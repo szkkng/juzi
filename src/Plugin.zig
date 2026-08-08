9ac7feb2892bf213724287c7e93b176e2750e400
Kengo Suzuki <git@kengo.dev>
2026-08-08 19:18:25 +0900

    Refactor plugin steps

--- src/Plugin.zig
+++ src/Plugin.zig
@@ -125,51 +125,34 @@ pub fn finalize(self: *Plugin) Result {
     for (config.formats) |format| {
         switch (format) {
             .vst3 => {
-                var flags: std.ArrayList([]const u8) = .empty;
-                flags.appendSlice(b.allocator, required_flags.cxx) catch @panic("OOM");
-
-                const vst3_module = b.createModule(.{
-                    .target = target,
-                    .optimize = optimize,
-                });
-                setup.addStandardDefs(vst3_module, optimize);
-                setup.addPluginDefinitions(vst3_module, &.{.vst3});
-                vst3_module.addIncludePath(juce.path("modules"));
-                vst3_module.addIncludePath(juce.path("modules/juce_audio_processors_headless/format_types"));
-                vst3_module.addIncludePath(juce.path("modules/juce_audio_processors_headless/format_types/VST3_SDK"));
-
-                const is_darwin = target.result.os.tag.isDarwin();
-                vst3_module.addCSourceFiles(.{
-                    .root = juce.path("modules"),
-                    .files = &.{b.fmt(
-                        "juce_audio_plugin_client/juce_audio_plugin_client_VST3.{s}",
-                        .{if (is_darwin) "mm" else "cpp"},
-                    )},
-                    .flags = flags.items,
-                });
-
-                switch (target.result.os.tag) {
-                    .macos => darwin.addSdkPaths(b, vst3_module),
-                    .windows => vst3_module.linkSystemLibrary("oldnames", .{}),
-                    else => {},
-                }
-
                 const vst3_step = b.step("vst3", "Build VST3");
 
                 // macOS: Zig doesn’t yet emit Mach‑O bundles (MH_BUNDLE) (see https://github.com/ziglang/zig/issues/14757).
                 // We build an MH_DYLIB instead and install it as the bundle’s main binary.
-                // Many hosts may accept this, but MH_BUNDLE is the conventional VST3 format.
-                // Once Zig supports MH_BUNDLE, switch to it and remove this workaround.
                 const vst3 = b.addLibrary(.{
                     .linkage = .dynamic,
                     .name = b.fmt("{s}", .{config.product_name}),
-                    .root_module = vst3_module,
+                    .root_module = b.createModule(.{ .target = target, .optimize = optimize }),
                     .win32_manifest = windows.createManifest(b),
                 });
+                setup.addStandardDefs(vst3.root_module, optimize);
+                setup.addPluginDefinitions(vst3.root_module, &.{.vst3});
                 vst3.root_module.linkLibrary(plugin_shared_lib);
+                vst3.root_module.addIncludePath(juce.path("modules"));
+                vst3.root_module.addIncludePath(juce.path("modules/juce_audio_processors_headless/format_types"));
+                vst3.root_module.addIncludePath(juce.path("modules/juce_audio_processors_headless/format_types/VST3_SDK"));
+                vst3.root_module.addCSourceFiles(.{
+                    .root = juce.path("modules/juce_audio_plugin_client"),
+                    .files = &.{b.fmt(
+                        "juce_audio_plugin_client_VST3.{s}",
+                        .{if (target.result.os.tag.isDarwin()) "mm" else "cpp"},
+                    )},
+                    .flags = required_flags.cxx,
+                });
 
                 switch (target.result.os.tag) {
                     .macos => {
+                        darwin.addSdkPaths(b, vst3.root_module);
                         const install_vst3 = darwin.addInstallBundle(vst3, .{ .plugin = .vst3 });
                         const codesign_run = darwin.addAdhocCodesign(
                             b,
@@ -195,6 +178,8 @@ pub fn finalize(self: *Plugin) Result {
                         vst3_step.dependOn(&install_vst3.step);
                     },
                     .windows => {
+                        vst3.root_module.linkSystemLibrary("oldnames", .{});
+                        windows.addResourcesRc(vst3.root_module, juceaide, info_text_file);
                         const arch_str = switch (target.result.cpu.arch) {
                             .x86_64 => "x86_64",
                             .x86 => "x86",
@@ -209,10 +194,9 @@ pub fn finalize(self: *Plugin) Result {
                             .dest_dir = .{ .override = .{ .custom = bundle_subpath } },
                             .dest_sub_path = b.fmt("{s}.vst3", .{vst3.name}),
                         });
-                        windows.addResourcesRc(vst3.root_module, juceaide, info_text_file);
                         vst3_step.dependOn(&install_vst3.step);
                     },
-                    else => @panic("Not implemented yet"),
+                    else => @panic("unsupported os"),
                 }
 
                 if (config.vst3_auto_manifest) {
@@ -223,7 +207,7 @@ pub fn finalize(self: *Plugin) Result {
                         .{
                             .target = target,
                             .optimize = optimize,
-                            .flags = flags.items,
+                            .flags = required_flags.cxx,
                         },
                     );
                     vst3_step.dependOn(&install_module_info.step);
@@ -237,42 +221,29 @@ pub fn finalize(self: *Plugin) Result {
                     continue;
                 }
 
-                var flags: std.ArrayList([]const u8) = .empty;
-                flags.appendSlice(b.allocator, required_flags.cxx) catch @panic("OOM");
-
-                const au_module = b.createModule(.{
-                    .target = target,
-                    .optimize = optimize,
-                });
-                setup.addStandardDefs(au_module, optimize);
-                setup.addPluginDefinitions(au_module, &.{.au});
-                au_module.addIncludePath(juce.path("modules"));
-                au_module.addIncludePath(juce.path("modules/juce_audio_plugin_client/AU"));
-                au_module.addCSourceFiles(.{
-                    .root = juce.path("modules"),
-                    .files = &.{
-                        "juce_audio_plugin_client/juce_audio_plugin_client_AU_1.mm",
-                        "juce_audio_plugin_client/juce_audio_plugin_client_AU_2.mm",
-                    },
-                    .flags = flags.items,
-                });
-
-                if (target.result.os.tag.isDarwin()) {
-                    darwin.addSdkPaths(b, au_module);
-                }
-
                 const au_step = b.step("au", "Build AU");
 
                 // macOS: Zig doesn’t yet emit Mach‑O bundles (MH_BUNDLE) (see https://github.com/ziglang/zig/issues/14757).
                 // We build an MH_DYLIB instead and install it as the bundle’s main binary.
-                // Many hosts may accept this, but MH_BUNDLE is the conventional AU format.
-                // Once Zig supports MH_BUNDLE, switch to it and remove this workaround.
                 const au = b.addLibrary(.{
                     .linkage = .dynamic,
                     .name = b.fmt("{s}", .{config.product_name}),
-                    .root_module = au_module,
+                    .root_module = b.createModule(.{ .target = target, .optimize = optimize }),
                 });
+                darwin.addSdkPaths(b, au.root_module);
+                setup.addStandardDefs(au.root_module, optimize);
+                setup.addPluginDefinitions(au.root_module, &.{.au});
                 au.root_module.linkLibrary(plugin_shared_lib);
+                au.root_module.addIncludePath(juce.path("modules"));
+                au.root_module.addIncludePath(juce.path("modules/juce_audio_plugin_client/AU"));
+                au.root_module.addCSourceFiles(.{
+                    .root = juce.path("modules/juce_audio_plugin_client"),
+                    .files = &.{
+                        "juce_audio_plugin_client_AU_1.mm",
+                        "juce_audio_plugin_client_AU_2.mm",
+                    },
+                    .flags = required_flags.cxx,
+                });
 
                 const install_au = darwin.addInstallBundle(au, .{ .plugin = .au });
                 const codesign_run = darwin.addAdhocCodesign(
@@ -291,39 +262,25 @@ pub fn finalize(self: *Plugin) Result {
                 result.install_steps.put(b.allocator, .au, au_step) catch @panic("OOM");
             },
             .standalone => {
-                var flags: std.ArrayList([]const u8) = .empty;
-                flags.appendSlice(b.allocator, required_flags.cxx) catch @panic("OOM");
-
-                const standalone_module = b.createModule(.{
-                    .target = target,
-                    .optimize = optimize,
-                });
-                setup.addStandardDefs(standalone_module, optimize);
-                setup.addPluginDefinitions(standalone_module, &.{.standalone});
-                standalone_module.addIncludePath(juce.path("modules"));
-                standalone_module.addCSourceFiles(.{
-                    .root = juce.path("modules"),
-                    .files = &.{
-                        "juce_audio_plugin_client/juce_audio_plugin_client_Standalone.cpp",
-                    },
-                    .flags = flags.items,
-                });
-
-                if (target.result.os.tag.isDarwin()) {
-                    darwin.addSdkPaths(b, standalone_module);
-                }
-
+                const standalone_step = b.step("standalone", "Build standalone");
                 const standalone = b.addExecutable(.{
                     .name = config.product_name,
-                    .root_module = standalone_module,
+                    .root_module = b.createModule(.{ .target = target, .optimize = optimize }),
                     .win32_manifest = windows.createManifest(b),
                 });
+                setup.addStandardDefs(standalone.root_module, optimize);
+                setup.addPluginDefinitions(standalone.root_module, &.{.standalone});
                 standalone.root_module.linkLibrary(plugin_shared_lib);
-
-                const standalone_step = b.step("standalone", "Build standalone");
+                standalone.root_module.addIncludePath(juce.path("modules"));
+                standalone.root_module.addCSourceFiles(.{
+                    .root = juce.path("modules/juce_audio_plugin_client"),
+                    .files = &.{"juce_audio_plugin_client_Standalone.cpp"},
+                    .flags = required_flags.cxx,
+                });
 
                 switch (target.result.os.tag) {
                     .macos => {
+                        darwin.addSdkPaths(b, standalone.root_module);
                         const install_standalone = darwin.addInstallBundle(standalone, .{ .plugin = .standalone });
                         const install_plist = darwin.addInstallInfoPlist(juceaide, info_text_file, config, .{ .plugin = .standalone });
                         const install_pkginfo = darwin.addInstallPkgInfo(juceaide, config.product_name, .{ .plugin = .standalone });
@@ -343,7 +300,7 @@ pub fn finalize(self: *Plugin) Result {
                         const install_standalone = b.addInstallArtifact(standalone, .{});
                         standalone_step.dependOn(&install_standalone.step);
                     },
-                    else => @panic("Not implemented yet"),
+                    else => @panic("unsupported os"),
                 }
 
                 const run_cmd = b.addRunArtifact(standalone);
@@ -354,7 +311,6 @@ pub fn finalize(self: *Plugin) Result {
                 result.artifacts.put(b.allocator, .standalone, standalone) catch @panic("OOM");
                 result.install_steps.put(b.allocator, .standalone, standalone_step) catch @panic("OOM");
             },
-            // else => @panic("Not implemented yet"),
         }
     }
 
